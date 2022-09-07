298862b09e8886c845acb163e8e07aeff5be5690
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-09-07 14:24:13 -0700

    Add image loading

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,79 @@
-use iced::{image, widget::button, Alignment, Button, Element, Row, Sandbox, Settings, Text};
+use iced::{
+    image, widget::button, Alignment, Application, Button, Column, Command, Element, Row, Settings,
+    Text,
+};
 
 fn main() -> iced::Result {
     GifProject::run(Settings::default())
 }
 
-struct GifProject {
-    // A vector of GIF image frames
-    frames: Vec<image::Handle>,
-
-    // The index of the frame being operated on
-    current: Option<u32>,
-
-    step_forward_button: button::State,
-    step_backward_button: button::State,
+#[derive(Debug, Clone)]
+enum GifProject {
+    NoProject,
+    Editing {
+        // A vector of GIF image frames
+        frames: Vec<image::Handle>,
+        frame_viewer: image::viewer::State,
+        // The index of the frame being operated on
+        current: i32,
+        step_forward_button: button::State,
+        step_backward_button: button::State,
+    },
+    Errored,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum Message {
     FrameStepForward,
     FrameStepBackward,
+    ProjectLoaded(Result<GifProject, Error>),
 }
 
-impl Sandbox for GifProject {
-    type Message = Message;
+#[derive(Debug, Clone)]
+enum Error {
+    RequestError,
+}
 
-    fn new() -> Self {
-        Self {
-            frames: Vec::new(),
-            current: None,
+impl From<reqwest::Error> for Error {
+    fn from(error: reqwest::Error) -> Error {
+        dbg!(error);
+
+        Error::RequestError
+    }
+}
+
+impl GifProject {
+    async fn init() -> Result<Self, Error> {
+        let project_file = Self::fetch_image(
+            "https://cdn.discordapp.com/emojis/643066491494727680.gif?size=128&quality=lossless",
+        )
+        .await?;
+
+        Ok(Self::Editing {
+            frames: vec![project_file],
+            frame_viewer: image::viewer::State::new(),
+            current: -1,
             step_forward_button: button::State::default(),
             step_backward_button: button::State::default(),
-        }
+        })
+    }
+
+    async fn fetch_image(url: &str) -> Result<image::Handle, reqwest::Error> {
+        let bytes = reqwest::get(url).await?.bytes().await?;
+        Ok(image::Handle::from_memory(bytes.as_ref().to_vec()))
+    }
+}
+
+impl Application for GifProject {
+    type Executor = iced::executor::Default;
+    type Message = Message;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Message>) {
+        (
+            Self::NoProject,
+            Command::perform(Self::init(), Message::ProjectLoaded),
+        )
     }
 
     fn title(&self) -> String {
@@ -38,26 +81,87 @@ impl Sandbox for GifProject {
     }
 
     fn view(&mut self) -> Element<Message> {
-        Row::new()
-            .padding(20)
-            .align_items(Alignment::Center)
-            .push(
-                Button::new(&mut self.step_backward_button, Text::new("<<"))
-                    .on_press(Message::FrameStepBackward),
-            )
-            .push(Text::new("ok").size(50))
-            .push(
-                Button::new(&mut self.step_forward_button, Text::new(">>"))
-                    .on_press(Message::FrameStepForward),
-            )
-            .into()
+        match self {
+            Self::Editing {
+                frames,
+                frame_viewer,
+                current,
+                step_backward_button,
+                step_forward_button,
+                ..
+            } => Column::new()
+                .padding(20)
+                .push(Row::new().push(image::Viewer::new(frame_viewer, frames[0].clone())))
+                .push(
+                    Row::new()
+                        .padding(20)
+                        .align_items(Alignment::Center)
+                        .push(
+                            Button::new(step_backward_button, Text::new("<<"))
+                                .on_press(Message::FrameStepBackward),
+                        )
+                        .push(Text::new(current.to_string()).size(50))
+                        .push(
+                            Button::new(step_forward_button, Text::new(">>"))
+                                .on_press(Message::FrameStepForward),
+                        ),
+                )
+                .into(),
+            _ => Row::new().into(),
+        }
     }
 
-    fn update(&mut self, message: Message) {
-        match (message, self.current) {
-            (Message::FrameStepForward, Some(x)) => self.current = Some(x + 1),
-            (Message::FrameStepBackward, Some(x)) => self.current = Some(x - 1),
-            _ => (),
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::FrameStepForward => match self {
+                Self::Editing {
+                    current,
+                    frames,
+                    frame_viewer,
+                    step_forward_button,
+                    step_backward_button,
+                } => {
+                    *self = Self::Editing {
+                        current: *current + 1,
+                        frames: frames.to_vec(),
+                        frame_viewer: *frame_viewer,
+                        step_forward_button: *step_forward_button,
+                        step_backward_button: *step_backward_button,
+                    };
+
+                    Command::none()
+                }
+                _ => Command::none(),
+            },
+            Message::FrameStepBackward => match self {
+                Self::Editing {
+                    current,
+                    frames,
+                    frame_viewer,
+                    step_forward_button,
+                    step_backward_button,
+                } => {
+                    *self = Self::Editing {
+                        current: *current - 1,
+                        frames: frames.to_vec(),
+                        frame_viewer: *frame_viewer,
+                        step_forward_button: *step_forward_button,
+                        step_backward_button: *step_backward_button,
+                    };
+
+                    Command::none()
+                }
+                _ => Command::none(),
+            },
+            Message::ProjectLoaded(Ok(project)) => {
+                *self = project;
+                Command::none()
+            }
+            Message::ProjectLoaded(Err(_error)) => {
+                *self = Self::Errored;
+
+                Command::none()
+            }
         }
     }
 }
